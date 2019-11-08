e596ca9eb04464737f0e4c678c4a910f87d6f712
Jean Abreu <30299701+jeabreu@users.noreply.github.com>
2019-11-08 20:27:39 -0300

    Terminando o dashboard

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,8 +1,8 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.18
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    reserved.
+  </footer>
 
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -1,11 +1,11 @@
 <?php
-    if($this->session->userdata('logged_in')){
+    if($this->session->userdata("logged_in")){
         
-        $tela ='';
+        //$tela ="";
         if(isset($tela)){
             $tela = $tela;
         } else{
-            $tela='view_dashboard';
+            $tela="view_dashboard";
         }
         //var_dump($tela);
 ?>
@@ -18,18 +18,18 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/bootstrap/bootstrap.min.css");?>">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/font-awesome/css/font-awesome.min.css");?>">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/Ionicons/css/ionicons.min.css");?>">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/jvectormap/jquery-jvectormap.css");?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/AdminLTE.min.css");?>">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="<?php echo site_url("assetsadm/css/skins/_all-skins.min.css");?>">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -44,53 +44,51 @@
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
     
+            <?php               
+            $this->load->view("template/header");
+            $this->load->view("template/topbar");
+            $this->load->view("template/sidebar");
+            $this->load->view("template/configbar");
+            if ($tela != "") {
+               $this->load->view("telas/" . $tela);
+            }
+            $this->load->view("template/footer");
+            $this->load->view("template/controlbar");
+            $this->load->view("template/js");
+            
+            ?>
+            <!-- Add the sidebar's background. This div must be placed
+             immediately after the control sidebar -->
+            <div class="control-sidebar-bg"></div>
+        </div>
+        <!-- ./wrapper -->  
     
-    <div class="wrapper">
-   <?php
-    $this->load->view("template/header");
-    $this->load->view("template/topbar");
-     $this->load->view("template/sidebar");
-     $this->load->view("template/configbar");
-     
-     if($tela=''){
-         $this->load->view('telas/'.$tela);
-     }
-     
-   $this->load->view("template/js");
-   $this->load->view("template/footer");
-   
-    ?>
-    
-    
-    
-    
-    
-  <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+ <!-- jQuery 3 -->
+<script src="<?php echo site_url("assetsadm/js/jquery/jquery.min.js");?>"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo site_url("assetsadm/js/bootstrap/bootstrap.min.js");?>"></script>
 <!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo site_url("assetsadm/js/jsfastclick/fastclick.js");?>"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?php echo site_url("assetsadm/js/adminlte.min.js");?>"></script>
 <!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?php echo site_url("assetsadm/js/jquery-sparkline/jquery.sparkline.min.js");?>"></script>
 <!-- jvectormap  -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?php echo site_url("assetsadm/js/jvectormap/jquery-jvectormap-1.2.2.min.js");?>"></script>
+<script src="<?php echo site_url("assetsadm/js/jvectormap/jquery-jvectormap-world-mill-en.js");?>"></script>
 <!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo site_url("assetsadm/js/jquery-slimscroll/jquery.slimscroll.min.js");?>"></script>
 <!-- ChartJS -->
-<script src="bower_components/chart.js/Chart.js"></script>
+<script src="<?php echo site_url("assetsadm/js/chart.js/Chart.js");?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard2.js"></script>
+<script src="<?php echo site_url("assetsadm/js/pages/dashboard2.js");?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="<?php echo site_url("assetsadm/js/demo.js");?>"></script>
 </body>
 </html>
 
 <?php
     }else{
-        redirect('login');
+        redirect("login");
     }
 ?>
